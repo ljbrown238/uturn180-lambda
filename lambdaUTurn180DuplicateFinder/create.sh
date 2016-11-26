eb8f3930e324f9e8918c77da7cda76411943a4ba
Loren J. Brown <ljbrown@quantumpeg.com>
2016-11-26 14:43:29 -0800

    Specify Node.js version in create.sh.

--- lambdaUTurn180DuplicateFinder/create.sh
+++ lambdaUTurn180DuplicateFinder/create.sh
@@ -4,6 +4,6 @@ aws lambda create-function \
 --function-name lambdaUTurn180DuplicateFinder \
 --zip-file fileb://./index.zip \
 --role arn:aws:iam::311492696836:role/lambda_sns_ses_s3 --handler index.handler \
---runtime nodejs \
+--runtime "nodejs4.3" \
 --profile gdl_uturn180 \
 --timeout 60
